288b1dadf9d5b70055b0b26cc404ac781a1940a2
sarinpost <30475718+sarinpost@users.noreply.github.com>
2021-08-06 20:17:46 +0700

    com

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const request = require('postman-request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// const url1 = "http://api.weatherstack.com/current?access_key=a97003635def866626b7144ece6bc89d&query=37.83,-122.42&units=f"
-// request({
-//     url: url1,
-//     json: true,
-// }, (error, response) => {
-//     const current = response.body.current
-//     console.log(current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. It feels like " + current.feelslike)
-// })
+const address = process.argv[2]
 
-const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2FyaW5wb3N0IiwiYSI6ImNrcncxdWIyMTBjYjUyb25wZWg1NXlqeW0ifQ.8TJjQZZrCsbkUFuwcnq80A&limit=1"
-request({
-    url: geocodeURL,
-    json: true,
-}, (error, response) => {
-    const [longitude, latitude] = response.body.features[0].center
-    console.log(latitude, longitude);
-})
+if (!address) {
+    console.log('Please provide an address such as "New york"')
+} else {
+    geocode(process.argv[2], (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return console.log(error);
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log(location);
+            console.log(forecastData);
+        })
+    })
+}
